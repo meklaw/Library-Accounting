005416afbb5d6325818e73f4221e9549d1cbd14e
Никита Ульянов <nik.ule@yandex.ru>
2022-09-11 17:05:47 +0300

    Initial commit

---
+++ src/main/java/ru/meklaw/app/dao/PersonDAO.java
@@ -0,0 +1,43 @@
+package ru.meklaw.app.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.meklaw.app.models.Person;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person;", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?;", new Object[]{id}, new PersonMapper())
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(id, name, age, email) VALUES (1, ?, ?,?)",
+                 person.getName(), person.getAge(), person.getEmail());
+    }
+
+    public void update(int id, Person person) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id = ?;",
+                person.getName(), person.getAge(), person.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE  id=?;", id);
+    }
+}
