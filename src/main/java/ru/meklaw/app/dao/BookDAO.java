202bd942984b50389585dcede9f3f154a2627c88
Никита Ульянов <nik.ule@yandex.ru>
2022-10-20 22:06:43 +0300

    add: index books

---
+++ src/main/java/ru/meklaw/app/dao/BookDAO.java
@@ -0,0 +1,23 @@
+package ru.meklaw.app.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.meklaw.app.models.Book;
+
+import java.util.List;
+
+@Component
+public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index() {
+        return jdbcTemplate.query("select * from book", new BookMapper());
+    }
+}
