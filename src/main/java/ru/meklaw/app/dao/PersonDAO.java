f973be6a9adb37d91d4a7ca01f29f8e93a996354
Никита Ульянов <nik.ule@yandex.ru>
2022-10-20 17:08:06 +0300

    fix: sql

--- src/main/java/ru/meklaw/app/dao/PersonDAO.java
+++ src/main/java/ru/meklaw/app/dao/PersonDAO.java
@@ -1,18 +1,13 @@
 package ru.meklaw.app.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.meklaw.app.models.Person;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 @Component
 public class PersonDAO {
@@ -33,74 +28,17 @@ public class PersonDAO {
                 .stream().findAny();
     }
 
-    public Optional<Person> show(String email) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE email = ?;", new Object[]{email}, new PersonMapper())
-                .stream().findAny();
-    }
-
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES (?, ?, ?, ?)",
-                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
+        jdbcTemplate.update("INSERT INTO Person(full_name, age) VALUES (?, ?)",
+                person.getFullName(), person.getAge());
     }
 
     public void update(int id, Person person) {
-        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE id = ?;",
-                person.getName(), person.getAge(), person.getEmail(), person.getAddress(), id);
+        jdbcTemplate.update("UPDATE Person SET full_name=?, age=? WHERE id = ?;",
+                person.getFullName(), person.getAge(), id);
     }
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE  id=?;", id);
     }
-
-    //тестируем производительность Пакетной вставки
-
-    public void testMultipleUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-        people.forEach(
-                person -> jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES (?, ?,?)",
-                        person.getName(), person.getAge(), person.getEmail())
-        );
-
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time: " + (after - before));
-    }
-
-    public void testBatchUpdate() {
-        List<Person> people = create1000People();
-        long before = System.currentTimeMillis();
-
-        jdbcTemplate.batchUpdate("INSERT INTO Person(name , age, email) VALUES (?, ?,?)",
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setString(1, people.get(i).getName());
-                        ps.setInt(2, people.get(i).getAge());
-                        ps.setString(3, people.get(i).getEmail());
-                    }
-
-                    @Override
-                    public int getBatchSize() {
-                        return people.size();
-                    }
-                });
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time: " + (after - before));
-    }
-
-    private List<Person> create1000People() {
-        List<Person> people = new ArrayList<>();
-
-        IntStream.range(0, 1000)
-                .forEach(value -> people.add(new Person(
-                        value, "Name " + value, 30, "test" + value + "@mail.ru", "some address"
-                )));
-
-        return people;
-    }
-
 }
