a998fad622f17a20d1b62bbbd6a3f69da5eb2ab5
Никита Ульянов <nik.ule@yandex.ru>
2022-10-21 13:26:08 +0300

    add: show book by id

--- src/main/java/ru/meklaw/app/controllers/BooksController.java
+++ src/main/java/ru/meklaw/app/controllers/BooksController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.meklaw.app.dao.BookDAO;
+import ru.meklaw.app.models.Book;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/books")
@@ -22,5 +26,14 @@ public class BooksController {
     public String index(Model model) {
         model.addAttribute("books", bookDAO.index());
         return "books/index";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        Optional<Book> showBook = bookDAO.show(id);
+        if (showBook.isEmpty())
+            return "books/show_error";
+
+        model.addAttribute("book", showBook.get());
+        return "books/show";
     }
 }
