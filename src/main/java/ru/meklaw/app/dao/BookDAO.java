1e7edf5532f98b6e4f94eeb05a73a5031c960a92
Никита Ульянов <nik.ule@yandex.ru>
2022-10-21 13:25:07 +0300

    add: show by id

--- src/main/java/ru/meklaw/app/dao/BookDAO.java
+++ src/main/java/ru/meklaw/app/dao/BookDAO.java
@@ -1,12 +1,12 @@
 package ru.meklaw.app.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.meklaw.app.models.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
@@ -19,5 +19,10 @@ public class BookDAO {
 
     public List<Book> index() {
         return jdbcTemplate.query("select * from book", new BookMapper());
+    }
+
+    public Optional<Book> show(int id) {
+        return jdbcTemplate.query("select * from book where id = ?", new Object[]{id}, new BookMapper())
+                .stream().findAny();
     }
 }
