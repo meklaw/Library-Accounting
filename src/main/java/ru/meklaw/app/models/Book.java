2d8ee84313dc972c514b80d7fcf46a9e8414fd52
Никита Ульянов <nik.ule@yandex.ru>
2022-10-24 00:25:41 +0300

    add: validation

--- src/main/java/ru/meklaw/app/models/Book.java
+++ src/main/java/ru/meklaw/app/models/Book.java
@@ -1,9 +1,18 @@
 package ru.meklaw.app.models;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Book {
     private int id;
+    @NotEmpty(message = "Название не должно быть пустым")
+    @Size(min = 1, max = 120, message = "Название должно быть от 1 до 120 символов")
     private String name;
+    @NotEmpty(message = "Автор не должен быть пустым")
+    @Size(min = 3, max = 120, message = "Автор должен быть от 3 до 120 символов")
     private String author;
+    @Min(value = 0, message = "Год издания не должен быть меньше 0")
     private int year;
     private int personId;
 
