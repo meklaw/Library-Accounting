49091d92afe5e21318fa24c1b0ea076e5c53a043
Никита Ульянов <nik.ule@yandex.ru>
2022-11-02 19:06:48 +0300

    moving to spring boot

---
+++ src/main/java/ru/meklaw/app/LibraryAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.meklaw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/meklaw/app/LibraryAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.meklaw.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
